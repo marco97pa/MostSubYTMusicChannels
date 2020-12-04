fbb3b49d9dca3dee974540d1c1d66ca07f8e94eb
marco97pa <marco97pa@live.it>
2020-12-04 11:56:10 +0100

    Solved a bug in messages.ts
    The last item of the list was empty
    Added comments to make code more readable

--- webapp/src/data/messages.ts
+++ webapp/src/data/messages.ts
@@ -5,25 +5,36 @@ export interface Message {
   id: number;
 }
 
+//Declare variables and constants
 const Papa = require('papaparse')
-
+const url = "https://raw.githubusercontent.com/marco97pa/MostSubYTMusicChannels/master/channels.csv";
 let messages: Message[];
 
+/* getDataFromCSV
+ * url: string -> url that points to the CSV file
+ *
+ * Uses [Papaparse](https://www.papaparse.com/) to parse a CSV file, given an URL
+ * Shows the data in log and returns an array of objects
+ */
 function getDataFromCSV(url: string){
   Papa.parse(url, {
     download: true,
     header: true,
     complete: function(results: any) {
+      //Show results on the log (contains even more info debugging info than the array of objects itself)
       console.log(results);
+      //Remove the last item because it is empty
+      results.data.pop(); 
+      //Populate data
       messages = results.data;
     }
   })
 }
 
-const url = "https://raw.githubusercontent.com/marco97pa/MostSubYTMusicChannels/master/channels.csv";
-
+//MAIN
 getDataFromCSV(url);
 
+//Methods to be called by pages
 export const getMessages = () => messages;
 
 export const getMessage = (id: number) => messages.find(m => m.id === id);
