ae6b14330986e981f58b6075cf05c7c2bbe0d2cb
marco97pa <marco97pa@live.it>
2020-12-21 21:46:38 +0100

    Added documentation to the Python script and the Workflows

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ import datetime
 from operator import itemgetter
 import tweepy
 import os
-#from keys import youtube_key
-#from keys import twitter_keys
 
-#Get API key for YouTube
+# Get API key for YouTube
 youtube_api_key = os.environ.get('YOUTUBE_API_KEY')
 
-#Get Twitter API keys
+# Get Twitter API keys
 consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
 consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
 access_token = os.environ.get('TWITTER_ACCESS_KEY')
 access_token_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 
 def load_channels():
+    """ Loads channels list from file
+
+    Opens the channels.csv file, reads it and returns a dictionary from that file
+
+    Returns:
+        list: representing the list of channels
+    """
     try:
         with open("channels.csv") as file:
             channel_dict = csv.DictReader(file)
@@ -28,7 +33,15 @@ def load_channels():
         print("File could not be loaded")
         return None
 
+
 def write_channels(channels):
+    """ Writes channels list to file
+
+    Opens the channels.csv file and writes to it the dictonary of the channels
+
+    Args:
+        channels (dict): the list of channels, each one with name, username, id and subs properties
+    """
     keys = ["name", "username", "id", "subs"]
     with open("channels.csv", "w", newline='') as file:
         writer = csv.DictWriter(file, fieldnames=keys)
@@ -36,6 +49,16 @@ def write_channels(channels):
         writer.writerows(channels)
 
 def get_subscribers(channels):
+    """ Gets the number of subscribers of one or more channels
+
+    From the list of channels provided as argument, it takes the channel id and queries YouTube to get more info about the statistics of that channel(s)
+
+    Args:
+        channels (dict): the list of channels, each one with id property
+
+    Returns:
+        response: a list containing the statistics about the channels provided
+    """
     assert type(channels) != None, "List of channels empty or invalid"
     id_list = ""
     for channel in channels:
@@ -45,6 +68,18 @@ def get_subscribers(channels):
     return response
 
 def update_subscribers(response, channels):
+    """ Updates the number of subscribers of the channels and takes actions based on the changes
+
+    It merges changes from the list of channels and the response provided as arguments.
+    It checks if there is any change and takes the according actions such as invoke subs_notify_change().
+
+    Args:
+        channels (dict): the list of channels, each one with id and subs property
+        response (response): the response provided from YouTube, containing the statistics of the channels above
+
+    Returns:
+        list: a list containing the channels, updated with new values
+    """ 
     for item in response["items"]:
         for channel in channels:
             if channel["id"] == item["id"]:
@@ -54,19 +89,48 @@ def update_subscribers(response, channels):
     return channels
 
 def subs_notify_change(channel):
+    """ Takes actions on change of the number of subscriber of a channel
+
+    It invokes a post on Twitter and adds this change to the log
+
+    Args:
+        channels (dict): a single channel
+    """ 
     twitter_post("{} reached {} Million subscribers on YouTube\n@{} #music #youtube #stats".format(channel["name"], int(channel["subs"])/1000000, channel["username"]))
     log_message("{} reached {} Mln subs".format(channel["name"], int(channel["subs"])/1000000))
 
 def check_if_ordered(channels):
+    """ Checks if the list of channels is ordered
+    
+    If it is not, it invokes a Twitter post about this change: a channel "surpassed" another based on its subscribers count
+
+    Args:
+        channels (list): a list containing the channels
+    """
     for i in range(len(channels) - 1):
         if channels[i]["subs"] < channels[i + 1]["subs"]:
             twitter_post("{} is now the #{} most subscribed music channel on YouTube with {} Million subs, surpassing {}\n@{} #music #youtube #stats"
             .format(channels[i+1]["name"], i+1, int(channels[i+1]["subs"])/1000000, channels[i]["name"], channels[i+1]["username"]))
 
 def sort_channels(channels):
+    """ Orders the list of channels based on the subscribers count, from the highest to the lowest
+
+    Args:
+        channels (list): a list containing the channels
+
+    Returns:
+        list: a list containing the channels sorted
+    """
     return sorted(channels, key=itemgetter('subs'), reverse=True)
 
 def report(channels):
+    """ Setups a the report: a message containing the top channels based on the subs count.
+    
+    If the message exceeds the 280 chars limit, it is trimmed nicely to the last entry.
+
+    Args:
+        channels (list): a list containing the channels
+    """
     msg = "Weekly report top #youtube #music channels\n"
     for channel in channels:
         old_msg = msg 
@@ -77,6 +141,11 @@ def report(channels):
     twitter_post(msg[:-1])
 
 def twitter_post(message):
+    """ Post a message on Twitter (uses the Tweepy module)
+    
+    Args:
+        message (str): a string containing the message to be posted
+    """
     print(message+"\n")
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -84,15 +153,25 @@ def twitter_post(message):
     api.update_status(message)
 
 def log_message(message):
+    """ Appends a message in the log (the message.txt file)
+
+    This file's contents will be used as text of the commit of changes on GitHub
+    
+    Args:
+        message (str): a string containing the message to be logged
+    """
     with open("message.txt", "a") as myfile:
         myfile.write(message+"\n")
 
+# Main
 channels = load_channels()
 response = get_subscribers(channels)
 channels = update_subscribers(response, channels)
 check_if_ordered(channels)
 channels = sort_channels(channels)
 write_channels(channels)
+
+# If the -report argument is passed on script launch, generate the report
 if len(sys.argv) > 1:
     if sys.argv[1] == "-report" or sys.argv[1] == "-r":
         report(channels)
