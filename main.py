7979996d384c53cd0bd2f511e4379079d4160797
marco97pa <marco97pa@live.it>
2021-01-04 21:57:30 +0100

    Fixed crash on duplicate tweets
    Fixes Catch Tweet Duplicate Error #2
    Also reverted last change on channels.csv for testing purposes

--- main.py
+++ main.py
@@ -152,7 +152,10 @@ def twitter_post(message):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    api.update_status(message)
+    try:
+        api.update_status(message)
+    except tweepy.error.TweepError as error:
+        print("WARNING: Tweet NOT posted because " + str(error))
 
 def log_message(message):
     """ Appends a message in the log (the message.txt file)
